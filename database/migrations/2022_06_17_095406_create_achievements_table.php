f7fa54474bd99c845301399bc3183069d1c7f27c
ps217276 <ps217276@summacollege.nl>
2022-06-17 12:23:42 +0200

    migrations and seeders

---
+++ database/migrations/2022_06_17_095406_create_achievements_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('achievements', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->unsignedBigInteger('exercise_id')->nullable();
+            $table->foreign('exercise_id')->references('id')->on('exercises');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prestaties');
+    }
+};
